e8b1eb308eccd84b90562ab3cfcbc3b7455e2941
Srinivas291105 <146736980+Srinivas291105@users.noreply.github.com>
2026-02-26 21:16:42 +0530

    Refactor people list HTML structure
    Updated HTML structure and classes for people list items.

--- logic.js
+++ logic.js
@@ -11,16 +11,16 @@ for (let i = 0; i < 5; i++) {
     // Insert your static content
     newDiv.innerHTML = `
         <div class="peoplearea">
-        <div class="people_list">
+        <div class="people_list" role="button" tabindex="0" onclick="window.location.href='${fruits[i]}.html'">
             <div class="profileimg">
-                <img src="${logos[i]}">
+                <img src="${logos[i]}" class="pfpicons">
             </div>
             <div class="namelastmsg">
                 <p class="pername">${fruits[i]}<p>
                 <p class="tme">time here</p>
                 <p class="lsmsg">"Hello How are you doing"</p>
             </div>
-        </div>
+        </div>                                                          
         </div>
     `;
     
